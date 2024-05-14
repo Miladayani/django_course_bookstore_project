3e9dac884e14e8b65aef69ca38c66a383feb1563
Your Name <you@example.com>
2024-05-14 13:19:00 +0330

    add post create view

--- books/views.py
+++ books/views.py
@@ -12,3 +12,9 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = "books/book_detail.html"
+
+
+class BookCreateView(generic.edit.CreateView):
+    model = Book
+    fields = ['title', 'author', 'content', 'price']
+    template_name = "books/book_create.html"
