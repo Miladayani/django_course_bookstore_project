eb92bc73ccd66fd7549a8482e63c9982c19b35ee
Your Name <you@example.com>
2024-05-20 10:48:01 +0330

    add related to book fk in Comment model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from books.models import Book
+from books.models import Book, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'date_time')
+
 
 admin.site.register(Book)
+admin.site.register(Comment, CommentAdmin)
