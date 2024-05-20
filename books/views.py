1b318baa193b6b7fd95e6028abf66f35bab7c806
Your Name <you@example.com>
2024-05-20 11:09:44 +0330

    render book comments in book detail view

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from .models import Book
+from django.shortcuts import get_object_or_404, render
 
 
 class BookListView(generic.ListView):
@@ -10,9 +11,14 @@ class BookListView(generic.ListView):
     context_object_name = "books"
 
 
-class BookDetailView(generic.DetailView):
-    model = Book
-    template_name = "books/book_detail.html"
+# class BookDetailView(generic.DetailView):
+#     model = Book
+#     template_name = "books/book_detail.html"
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book_comments = book.comments.all()
+    return render(request, "books/book_detail.html", {"book": book, 'comments': book_comments})
 
 
 class BookCreateView(generic.CreateView):
