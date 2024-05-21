1c3a71c6b2a47ba3d9151391e47c3000f70c7215
Your Name <you@example.com>
2024-05-21 11:25:56 +0330

    add comment form to book detail view

--- books/admin.py
+++ books/admin.py
@@ -4,7 +4,7 @@ from books.models import Book, Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'book', 'text', 'date_time')
+    list_display = ('user', 'book', 'text', 'date_time', 'recommend', 'is_active')
 
 
 admin.site.register(Book)
